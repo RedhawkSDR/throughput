f2959213296acecd580c79467d9cd7c12b1cf330
Justin A. Irwin <jirwin@geontech.com>
2015-04-06 17:31:01 -0400

    Refactor reader to separate connection methods

--- reader.cc
+++ reader.cc
@@ -10,60 +10,74 @@
 #include <arpa/inet.h>
 #include <sys/un.h>
 
+int connect_unix(const std::string& address)
+{
+    int fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (fd < 0) {
+        perror("socket");
+        return -1;
+    }
+
+    struct sockaddr_un writer;
+    writer.sun_family = AF_UNIX;
+    strcpy(writer.sun_path, address.c_str());
+    int len = strlen(writer.sun_path) + sizeof(writer.sun_family);
+    if (writer.sun_path[0] == '@') {
+        writer.sun_path[0] = '\0';
+    }
+    if (connect(fd, (struct sockaddr*)&writer, len) < 0) {
+        perror("connect");
+        return -1;
+    }
+    return fd;
+}
+
+int connect_tcp(const std::string& address)
+{
+    int fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (fd < 0) {
+        perror("socket");
+        return -1;
+    }
+
+    struct sockaddr_in server;
+    memset(&server, 0, sizeof(sockaddr_in));
+    server.sin_family = AF_INET;
+    std::string::size_type isep = address.find(':');
+    server.sin_port = atoi(address.substr(isep+1).c_str());
+    inet_aton(address.substr(0, isep).c_str(), &server.sin_addr);
+
+    if (connect(fd, (struct sockaddr*)&server, sizeof(server)) < 0) {
+        perror("connect");
+        return -1;
+    }
+    return fd;
+}
+
+int connect(const std::string& protocol, const std::string& address)
+{
+    if (protocol == "unix") {
+        return connect_unix(address);
+    } else if (protocol == "tcp") {
+        return connect_tcp(address);
+    } else {
+        std::cerr << "Unknown protocol '" << protocol << "'" << std::endl;
+        return -1;
+    }
+}
+
 int main(int argc, const char* argv[])
 {
     if (argc < 4) {
         exit(1);
     }
 
-    int fd;
-
-    if (strcmp(argv[1], "unix") == 0) {
-        fd = socket(AF_UNIX, SOCK_STREAM, 0);
-        if (fd < 0) {
-            perror("socket");
-            exit(1);
-        }
-
-        struct sockaddr_un writer;
-        writer.sun_family = AF_UNIX;
-        strcpy(writer.sun_path, argv[2]);
-        int len = strlen(writer.sun_path) + sizeof(writer.sun_family);
-        if (writer.sun_path[0] == '@') {
-            writer.sun_path[0] = '\0';
-        }
-        if (connect(fd, (struct sockaddr*)&writer, len) < 0) {
-            perror("connect");
-            exit(1);
-        }
-    } else if (strcmp(argv[1], "tcp") == 0) {
-        fd = socket(AF_INET, SOCK_STREAM, 0);
-        if (fd < 0) {
-            perror("socket");
-        }
-
-        char* address = strdup(argv[2]);
-        char* isep = strchr(address, ':');
-        short port = atoi(isep+1);
-        *isep = '\0';
-
-        struct sockaddr_in server;
-        memset(&server, 0, sizeof(sockaddr_in));
-        server.sin_family = AF_INET;
-        server.sin_port = port;
-        inet_aton(address, &server.sin_addr);
-
-        if (connect(fd, (struct sockaddr*)&server, sizeof(server)) < 0) {
-            perror("connect");
-            exit(1);
-        }
-    } else {
-        std::cerr << "Unknown protocol '" << argv[1] << "'" << std::endl;
+    int fd = connect(argv[1], argv[2]);
+    if (fd < 0) {
         exit(1);
     }
 
     int bufsize = atoi(argv[3]);
-
     std::vector<char> buffer;
     buffer.resize(bufsize);
 
