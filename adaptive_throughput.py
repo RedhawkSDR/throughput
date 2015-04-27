d18c29090aa8de8fb76f4fe0b3539e99024c18b0
Justin A. Irwin <jirwin@geontech.com>
2015-04-27 17:03:15 -0400

    Use standard deviation of sample window instead of coarse ratio for rate stabilization; report best peak and average values

--- adaptive_throughput.py
+++ adaptive_throughput.py
@@ -2,6 +2,7 @@ import sys
 import time
 import getopt
 import math
+import numpy
 
 import numa
 import raw
@@ -46,13 +47,18 @@ if __name__ == '__main__':
     transport = 'unix'
     numa_distance = None
     data_format = 'octet'
-    poll_time = 0.1 #1.0
+    poll_time = 0.1
     window_size = 10
+    tolerance = 0.1
 
-    opts, args = getopt.getopt(sys.argv[1:], 'w:', ['transport=', 'interface=', 'numa-distance='])
+    opts, args = getopt.getopt(sys.argv[1:], 'w:t:d:', ['transport=', 'interface=', 'numa-distance='])
     for key, value in opts:
         if key == '-w':
             window_size = int(value)
+        elif key == '-t':
+            poll_time = float(value)
+        elif key == '-d':
+            tolerance = float(value)
         elif key == '--transport':
             transport = value
         elif key == '--numa-distance':
@@ -61,6 +67,7 @@ if __name__ == '__main__':
             interface = value
 
     numa_policy = numa.NumaPolicy(numa_distance)
+    max_window_size = window_size*2
 
     if interface == 'raw':
         factory = raw.factory(transport)
@@ -83,6 +90,9 @@ if __name__ == '__main__':
     best_rate = 0.0
     best_size = 0
 
+    peak_rate = 0.0
+    peak_size = 0
+
     while transfer_size < (64*1024*1024):
         time.sleep(poll_time)
         now = time.time()
@@ -95,32 +105,41 @@ if __name__ == '__main__':
         current_rate = delta / elapsed
         current.append(current_rate)
 
+        if current_rate > peak_rate:
+            peak_rate = current_rate
+            peak_size = transfer_size
+
         # Adapt transfer rate
         current = current[-window_size:]
-        average = sum(current)/len(current)
+        average = numpy.average(current)
         ratio = current_rate / average
+        # Get the normalized standard deviation
+        dev = numpy.std(current)/average
         if best_rate > 0.0:
             best_ratio = average/best_rate
         else:
-            best_ratio = 0.0
-        print to_gbps(current_rate), to_gbps(average), to_percent(ratio), to_percent(best_ratio)
+            best_ratio = 1.0
+        print to_gbps(current_rate), to_gbps(average), '%.2f' % best_ratio, '%.3f' % dev
         if len(current) < window_size:
             # Require minimum number of samples
             continue
-        if ratio > 1.02 or ratio < 0.98:
-            # Wait for measurements to stabilize
+        if len(current) < max_window_size and dev > tolerance:
+            # Try to wait for measurements to stabilize
             continue
         if average > best_rate:
             best_rate = average
             best_size = transfer_size
-        if best_ratio > 0.0 and best_ratio < 0.90:
+        if len(current) >= max_window_size:
+            break
+        if best_ratio < 0.90:
             break
         current = []
         transfer_size *= 2
         test.transfer_size(transfer_size)
         print 'Transfer size', to_binary(transfer_size)
 
-    print to_binary(best_size), to_gbps(best_rate)
-        
+    print 'Average:', to_binary(best_size), to_gbps(best_rate)
+    print 'Peak:   ', to_binary(peak_size), to_gbps(peak_rate)
+
     test.stop()
     test.terminate()
