b582f30495669aa2cd567dfbe0bb75278b0277fe
Justin A. Irwin <jirwin@geontech.com>
2015-04-28 13:26:53 -0400

    Collect statistical tracking and stability measurement into a class

--- adaptive_throughput.py
+++ adaptive_throughput.py
@@ -65,6 +65,34 @@ class AggregateTest(object):
             test.terminate()
 
 
+class SampleWindow(object):
+    def __init__(self, window_size, tolerance):
+        self.values = []
+        self.window_size = window_size
+        self.max_window_size = 2 * self.window_size
+        self.tolerance = tolerance
+
+    def add_sample(self, value):
+        self.values.append(value)
+
+    def is_stable(self):
+        if len(self.values) < self.window_size:
+            return False
+        elif len(self.values) >= self.max_window_size:
+            return True
+
+        return self.variance() <= self.tolerance
+
+    def average(self):
+        return numpy.average(self.values)
+
+    def variance(self):
+        return numpy.std(self.values)/self.average()
+
+    def reset(self):
+        self.values = []
+
+
 if __name__ == '__main__':
     transfer_size = 16*1024
     interface = 'raw'
@@ -92,7 +120,6 @@ if __name__ == '__main__':
             interface = value
 
     numa_policy = numa.NumaPolicy(numa_distance)
-    max_window_size = window_size*2
 
     if interface == 'raw':
         factory = raw.factory(transport)
@@ -100,6 +127,8 @@ if __name__ == '__main__':
         factory = corba.factory(transport)
     else:
         raise SystemExit('No interface '+interface)
+
+    stats = SampleWindow(window_size, tolerance)
 
     test = AggregateTest(factory, data_format, transfer_size, numa_policy, count)
     test.start()
@@ -109,8 +138,6 @@ if __name__ == '__main__':
     now = start
     last_time = start
     last_total = 0
-
-    current = []
 
     best_rate = 0.0
     best_size = 0
@@ -128,37 +155,29 @@ if __name__ == '__main__':
         delta = current_total - last_total
         last_total = current_total
         current_rate = delta / elapsed
-        current.append(current_rate)
+        stats.add_sample(current_rate)
 
         if current_rate > peak_rate:
             peak_rate = current_rate
             peak_size = transfer_size
 
         # Adapt transfer rate
-        current = current[-window_size:]
-        average = numpy.average(current)
+        average = stats.average()
         ratio = current_rate / average
         # Get the normalized standard deviation
-        dev = numpy.std(current)/average
         if best_rate > 0.0:
             best_ratio = average/best_rate
         else:
             best_ratio = 1.0
-        print to_gbps(current_rate), to_gbps(average), '%.2f' % best_ratio, '%.3f' % dev
-        if len(current) < window_size:
-            # Require minimum number of samples
+        print to_gbps(current_rate), to_gbps(average), '%.2f' % best_ratio, '%.3f' % stats.variance()
+        if not stats.is_stable():
             continue
-        if len(current) < max_window_size and dev > tolerance:
-            # Try to wait for measurements to stabilize
-            continue
-        if average > best_rate:
+        elif average >= best_rate:
             best_rate = average
             best_size = transfer_size
-        if len(current) >= max_window_size:
-            break
         if best_ratio < 0.90:
             break
-        current = []
+        stats.reset()
         transfer_size *= 2
         test.transfer_size(transfer_size)
         print 'Transfer size', to_binary(transfer_size)
